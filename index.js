91abfda307b4a17b8f2f98f9ec847e814a18a0cf
Gaurav Dubey <gaurav@steinnlabs.com>
2018-12-28 12:13:24 +0530

    express simple rest

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +9,58 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+//get method
+app.get('/dishes', (req, res, next) => {
+    res.end('will send all the dishes to you');
+});
+
+//post method
+app.post('/dishes', (req, res, next) => {
+    res.end('will add the dish:' + req.body.name + 'with details: ' + req.body.description);
+});
+
+//put method
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('put operation not supported on dishes');
+});
+
+//delete method
+app.delete('/dishes', (req, res, next) => {
+    res.end('deleting all the dishes');
+});
+
+
+//get method
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('will send details of the dish' + req.params.dishId + 'to you');
+});
+
+//post method
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('post operation not supported on /dishes/' + req.params.dishId);
+});
+
+//put method
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('updating the dish:' + req.params.dishId + '\n');
+    res.end('will update the dish:' + req.body.name + 'with details:' + req.body.description);
+});
+
+//delete method
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('deleting dish: ' + req.params.dishId);
+});
+
+
 
 app.use(express.static(__dirname + '/public'));
 
